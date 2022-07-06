8bf26cd8cbedfbe84a91ef258d09d4a4b16e4bd3
gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2022-07-05 22:38:50 -0400

    Working towards maybe a redistributable Python version.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Assume.py",
+    author="gatewaynode",
+    version="0.1",
+    install_requires=[
+        "black",
+        "boto3",
+        "botocore",
+        "cffi",
+        "click",
+        "cryptography",
+        "Jinja2",
+        "jmespath",
+        "MarkupSafe",
+        "mypy-extensions",
+        "pathspec",
+        "pdoc",
+        "platformdirs",
+        "pycparser",
+        "Pygments",
+        "python-dateutil",
+        "PyYAML",
+        "s3transfer",
+        "six",
+        "tomli",
+        "urllib3",
+    ],
+)
